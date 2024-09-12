5bbb8c79542ed24dcbeb70ad93a5f8478de94a49
NabhanAhmedSaid <100470531+NabhanAhmedSaid@users.noreply.github.com>
2024-09-12 12:16:51 +0400

    Add files via upload

---
+++ InfoSysCsharp/Program.cs
@@ -0,0 +1,11 @@
+﻿string os_version = Environment.OSVersion.ToString();
+System.Console.WriteLine($"OS : {os_version}");
+string MachineName = Environment.MachineName.ToString();
+System.Console.WriteLine($"MachineName : {MachineName}");
+string UserName = Environment.UserName.ToString();
+System.Console.WriteLine($"User Name : {UserName}");
+string ProcessorCount = Environment.ProcessorCount.ToString();
+System.Console.WriteLine($"ProcessorCount: {ProcessorCount}");
+string NetVersion = Environment.Version.ToString();
+System.Console.WriteLine($".Net Version : {NetVersion}");
+
